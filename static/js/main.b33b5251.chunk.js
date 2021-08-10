86f0d485e442ed53257cc7625b9d9ce0a0daed1a
Artem <temaqwerty15@gmail.com>
2021-08-10 22:13:32 +0300

    Updates

---
+++ static/js/main.b33b5251.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-starter-pack"]=this["webpackJsonpreact-starter-pack"]||[]).push([[0],{10:function(t,e,r){},11:function(t,e,r){"use strict";r.r(e);var c=r(1),n=r(0),s=r.n(n),a=r(3),i=r.n(a),o=(r(9),r(10),function(){return Object(c.jsx)("div",{className:"starter",children:"Start test Deploy"})});i.a.render(Object(c.jsx)(s.a.StrictMode,{children:Object(c.jsx)(o,{})}),document.getElementById("root"))},9:function(t,e,r){}},[[11,1,2]]]);
+//# sourceMappingURL=main.b33b5251.chunk.js.map
